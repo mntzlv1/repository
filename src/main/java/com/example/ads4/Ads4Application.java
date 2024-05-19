0d6fbcea08eb53b9ad682f70e58e1b9354998eef
Amina <231599@astanait.edu.kz>
2024-05-19 18:34:09 +0600

    first in it

---
+++ src/main/java/com/example/ads4/Ads4Application.java
@@ -0,0 +1,13 @@
+package com.example.ads4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ads4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ads4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ads4/Ads4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ads4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ads4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
